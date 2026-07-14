f101e627314b2546a93c4ff42f67080c986e162b
Bishab01 <bishabshakya40@gmail.com>
2026-07-14 23:02:21 +0545

    Implement code changes to enhance functionality and improve performance across various modules.

--- frontend/src/homepage/body/productCard.jsx
+++ frontend/src/homepage/body/productCard.jsx
@@ -1,15 +1,15 @@
 function Productcard({category, productName, image, productPrice }){
     return(
-        <div className="flex flex-col rounded-2xl duration-400 border-gray-300 border-2 overflow-hidden hover:shadow-xl w-70 h-100">
+        <div className="flex flex-col rounded-2xl duration-400 border-gray-300 border-2 overflow-hidden hover:shadow-xl w-[45%] sm:w-[30%] lg:w-[23%] xl:w-[18%] h-100">
             <div className="h-[65%]">
                 <img 
                     src={image}
                     alt="image of the product" 
-                    className="hover:scale-103 duration-400 object-fill h-full w-full"
+                    className="hover:scale-105 duration-400 object-fill h-full w-full"
                 />
             </div>
             <div className="px-3 py-4 border-gray-300 border-b-2 font-serif">
-                <p className="text-gray-500 ">{category}</p>
+                <p className="text-gray-600 ">{category}</p>
                 <p className="text-lg">{productName}</p>
             </div>
             <div className="p-3">
