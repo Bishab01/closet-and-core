9ab5d0aaf0efe71db404c5bd138133c388bbe357
Dikshan <limbudikshan123@gmail.com>
2026-08-06 15:35:23 +0545

    overlay

--- frontend/src/components/sidemenu.jsx
+++ frontend/src/components/sidemenu.jsx
@@ -1,8 +1,19 @@
 import { X } from "lucide-react";
 import { NavLink } from "react-router-dom";
 
+
+const CATEGORIES = ["ALL", "HEADWARE","FOOTWARE","CLOTHING"];
+
 function SideMenu({ isOpen, onClose }) {
-  return null;
+   return (
+        <>
+            <div
+                onClick={onClose}
+                className={`fixed inset-0 bg-black/40 z-40 transition-opacity duration-300 ${
+                    isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+                }`}
+            />
+)
 }
 
 export default SideMenu;
